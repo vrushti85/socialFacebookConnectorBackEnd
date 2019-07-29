0837df8883a97b7d2ce52ab61c97985f331c3719
vrushti patel <vrushti.patel.sa@gmail.com>
2019-07-29 19:39:22 +0530

    changes

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require("express");
-
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/facebookSocialCoonector", { useNewUrlParser: true });
 bodyParser = require('body-parser');
 var app = express();
-
 const cors = require('cors')
-const router=require("./routes/userRoutes")
+const router = require("./routes/userRoutes")
+
 var corsOptions = {
   origin: 'https://localhost:4200',
   optionsSuccessStatus: 200
 }
+
 app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
@@ -20,9 +20,6 @@ app.use(router)
 app.get("/", (req, res) => {
   res.send("successfully connected");
 });
-// var providerId;
-
-
 
 const port = process.env.port || 5000;
 app.listen(port, () => {
